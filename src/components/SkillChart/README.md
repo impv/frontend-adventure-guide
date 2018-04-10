47511655e9b914dcf5f208b78012c3ea71de795f
pocka <pocka.moe@gmail.com>
2018-04-10 19:47:23 +0900

    :sparlkes: Add <SkillChart>

---
+++ src/components/SkillChart/README.md
@@ -0,0 +1,12 @@
+```jsx
+initialState = {
+  skills: [
+    { skill: 'ビジュアル力', level: 0 },
+    { skill: '表現力', level: 4 },
+    { skill: '先端技術力', level: 2 },
+    { skill: 'アプリケーション力', level: 5 },
+    { skill: 'セマンティックWeb力', level: 3 }
+  ]
+}
+;<SkillChart skills={state.skills} />
+```
---
+++ src/components/SkillChart/index.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react'
+
+import {
+  Radar,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis
+} from 'recharts'
+
+import Skill from '~/types/Skill'
+
+export interface Props {
+  /** 必要なスキル一覧 */
+  skills: Skill[]
+}
+
+/** スキルグラフ */
+export const SkillChart: React.SFC<Props> = ({ skills }) => (
+  <RadarChart width={500} height={300} data={skills}>
+    <PolarGrid stroke="#fff" />
+    <PolarAngleAxis
+      dataKey="skill"
+      tickFormatter={x => x}
+      tick={{ fill: '#fff', fontSize: '0.8em' }}
+    />
+    <PolarRadiusAxis
+      domain={[0, 5]}
+      tickCount={6}
+      stroke="rgba(0,0,0,0)"
+      tickFormatter={x => x}
+    />
+    <Radar dataKey="level" stroke="#03D1AB" fill="#03D1AB" fillOpacity={0.5} />
+  </RadarChart>
+)
+
+export default SkillChart
