7529bf182f9ec740f118157509f72b27b279081a
pocka <pocka.moe@gmail.com>
2018-04-10 12:54:18 +0900

    :sparkles: Add container for <PurposeSelector>

--- src/pages/CheckList.tsx
+++ src/pages/CheckList.tsx
@@ -1,29 +1,18 @@
 import * as React from 'react'
 
 import Button from '~/components/Button'
-import CheckItem from '~/components/CheckItem'
 import Page from '~/components/Page'
 import PageFooter from '~/components/PageFooter'
 import PageHeader from '~/components/PageHeader'
 
 import { Link } from 'react-router-dom'
 
-import { purposes } from '~/config/purposes.yml'
+import PurposeSelector from '~/containers/PurposeSelector'
 
-const CheckItems: React.SFC = () => (
-  <div>
-    {purposes.map(({ purpose }) => (
-      <CheckItem key={purpose} checked>
-        {purpose}
-      </CheckItem>
-    ))}
-  </div>
-)
-
-const CheckList: React.StatelessComponent = () => (
+const CheckList: React.SFC = () => (
   <Page>
     <PageHeader iconText="?">あなたのやりたいことは?</PageHeader>
-    <CheckItems />
+    <PurposeSelector />
     <PageFooter>
       <Link to="/skill">
         <Button type="primary">つぎへ</Button>
