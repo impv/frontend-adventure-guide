2491adfab60b28755ea044804f5513f353d5fac9
pocka <pocka.moe@gmail.com>
2018-04-11 14:35:58 +0900

    :bug: Workaround for parcel packaging bug
    * Apply workaround for https://github.com/parcel-bundler/parcel/issues/933

--- src/store/modules/skillDetail/index.ts
+++ src/store/modules/skillDetail/index.ts
@@ -1,3 +1,7 @@
+// Workaround for parcel's JSPackager bug.
+// TODO: Remove when https://github.com/parcel-bundler/parcel/pull/1011 is merged
+const moduleName = '~/store/modules/skillDetail'
+
 export { default } from './reducer'
 
 export * from './actions'
