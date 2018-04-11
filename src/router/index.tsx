057ee37b7f8400a52e19181268af68ea945c3329
pocka <pocka.moe@gmail.com>
2018-04-11 12:04:53 +0900

    :sparkles: Add navigation guard

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,20 +1,20 @@
 import * as React from 'react'
 
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Redirect, Route } from 'react-router-dom'
 
 import store from '~/store'
 
 import CheckList from '~/pages/CheckList'
 import SkillChart from '~/pages/SkillChart'
 import ToDo from '~/pages/ToDo'
-import ToDoDetails from '~/pages/ToDoDetails'
+import $ToDoDetails from '~/pages/ToDoDetails'
 
 export const ConnectedRouter: React.SFC = () => (
   <BrowserRouter>
     <div>
       <Route exact path="/" component={CheckList} />
-      <Route exact path="/skill" component={SkillChart} />
-      <Route exact path="/todo" component={ToDo} />
+      <Route exact path="/skill" component={PurposeSelectedGuard(SkillChart)} />
+      <Route exact path="/todo" component={PurposeSelectedGuard(ToDo)} />
       <Route
         exact
         path="/todo/:ability"
@@ -23,3 +23,15 @@ export const ConnectedRouter: React.SFC = () => (
     </div>
   </BrowserRouter>
 )
+
+// やりたいことが指定されていない場合にやりたいことリストへリダイレクトする
+const PurposeSelectedGuard: (
+  c: React.SFC<any>
+) => React.SFC<any> = Component => props =>
+  store.getState().purpose.purposes.filter(p => p.selected).length > 0 ? (
+    <Component {...props} />
+  ) : (
+    <Redirect to="/" />
+  )
+
+const ToDoDetails = PurposeSelectedGuard($ToDoDetails)
