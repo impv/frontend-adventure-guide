d685338a43f9bea4d0b9ffa498b5d7d0b162107d
pocka <pocka.moe@gmail.com>
2018-04-12 12:28:04 +0900

    :sparkles: Add <ToDoList>

---
+++ src/components/ToDoList/index.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react'
+
+import styled from 'styled-components'
+
+import AccordionList from '~/components/AccordionList'
+import AccordionItem from '~/components/AccordionItem'
+
+import ToDo from '~/types/ToDo'
+
+export interface Props {
+  /** やることのリスト */
+  todos: ToDo[]
+}
+
+/** やることリスト */
+export const ToDoList: React.SFC<Props> = ({ todos }) => (
+  <AccordionList>
+    {todos.map(todo => (
+      <AccordionItem title={todo.summary}>
+        <Description>{todo.description}</Description>
+      </AccordionItem>
+    ))}
+  </AccordionList>
+)
+
+export default ToDoList
+
+const Description = styled.div`
+  // misc
+  white-space: pre-line;
+`
