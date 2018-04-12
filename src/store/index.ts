5eab3e2ae7162e879da96ac98906040faca425bc
pocka <pocka.moe@gmail.com>
2018-04-12 14:50:25 +0900

    :sparkles: Persist redux store

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,9 @@ import { applyMiddleware, combineReducers, createStore } from 'redux'
 
 import { routerMiddleware, routerReducer } from 'react-router-redux'
 
+import { PersistConfig, persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import purpose, { State as Purpose } from './modules/purpose'
 import skillDetail, { State as SkillDetail } from './modules/skillDetail'
 
@@ -9,14 +12,27 @@ import { history } from '~/router/history'
 
 const middleware = routerMiddleware(history as any)
 
+const persistConfig: PersistConfig = {
+  storage,
+  key: 'root',
+  blacklist: ['router']
+}
+
 export interface RootState {
   purpose: Purpose
   skillDetail: SkillDetail
 }
 
 const store = createStore<RootState>(
-  combineReducers({ purpose, skillDetail, router: routerReducer }),
+  // Workaround for type error caused by Incorrect type definitions for redux-persist
+  // TODO: Remove as any when merged https://github.com/rt2zz/redux-persist/pull/778
+  persistReducer(
+    persistConfig,
+    combineReducers({ purpose, skillDetail, router: routerReducer })
+  ) as any,
   applyMiddleware(middleware)
 )
 
 export default store
+
+export const persistor = persistStore(store)
